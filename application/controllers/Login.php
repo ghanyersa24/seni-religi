8eef6dc99773971c5d202cb837a184ec4d21a10e
ghanyersa24 <ghanyersa24@gmail.com>
2020-02-11 17:01:32 +0700

    login pengurus

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -3,34 +3,18 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Login extends CI_Controller
 {
-	public function customer()
+	public function pengurus()
 	{
-		$username=post('username');
-		$do = $this->data_custom->login_customer($username);
-		if (is_null($do->data)) {
+		$username = post('nim');
+		$do = $this->data_model->select_one('pengurus', array('nim' => $username));
+		if ($do->error) {
 			error("username and password isn't match");
 		} else {
 			if (password_verify(post("password"), $do->data->password))
-				if ($do->data->status == 'activated') {
-					success("waiting a minute ...", $do->data);
-				} else
-					error("sorry, this account isn't activate");
-			else
-				error("username and password isn't match");
-		}
-	}
-	
-	public function admin()
-	{
-		$do = $this->data_model->select_one('zero', array('username' => post('username')));
-		if (is_null($do->data)) {
-			error("username and password isn't match");
-		} else {
-			if (password_verify(post("password"), $do->data->password))
-				if ($do->data->status == 'activated') {
-					success("waiting a minute ...", $do->data);
-				} else
-					error("sorry, this account isn't activate");
+				// if ($do->data->status == 'activated') {
+				success("waiting a minute ...", $do->data);
+			// } else
+			// error("sorry, this account isn't activate");
 			else
 				error("username and password isn't match");
 		}
